b5c2351088429093a7bb0aedc9efc8b9f9f2360e
Subin Yook <sprout.jessie@gmail.com>
2023-06-25 19:43:50 +0900

    백준 이진탐색 문제 풀이

---
+++ 00-basic/49-problem-2805.js
@@ -0,0 +1,24 @@
+// 2023-06-25
+// 이진탐색 - 나무자르기
+
+const [n, ...input] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const need = n.split(" ").map(Number)[1]
+const tree = input[0].split(" ").map(Number)
+
+let start = 1
+let end = Math.max(...tree)
+let result = 0
+while(start <= end) {
+    let mid = parseInt((start + end) / 2)
+    let total = 0
+    for (let i of tree) {
+        if (i > mid) total += i - mid
+    }
+    if (total >= need) {
+        result = mid
+        start = mid + 1
+    } else {
+        end = mid - 1
+    }
+}
+console.log(result)
---
+++ 00-basic/50-problem-1654.js
@@ -0,0 +1,24 @@
+// 2023-06-25
+// 이진탐색 - 랜선 자르기
+
+const [n, ...input] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const need = n.split(" ").map(Number)[1]
+const lines = input.map(Number)
+
+let start = 1
+let end = Math.max(...lines)
+let result = 0
+while (start <= end) {
+    let mid = parseInt((start + end) / 2)
+    let count = 0
+    for (let i of lines) {
+        count += parseInt(i / mid)
+    }
+    if (count >= need) {
+        result = mid
+        start = mid + 1
+    } else {
+        end = mid - 1
+    }
+}
+console.log(result)
