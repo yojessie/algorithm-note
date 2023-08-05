a27440e34aae4ca573441c43bb8af516cec72cd4
Subin Yook <sprout.jessie@gmail.com>
2023-08-05 16:42:28 +0900

    프로그래머스 레벨1문제 다시 풀기

--- 01-programmers-lv1/230804-키패드누르기.js
+++ 01-programmers-lv1/230804-키패드누르기.js
@@ -62,3 +62,56 @@ function solution(numbers, hand) {
 }
 
 // 2023-08-04
+// 코드 정리하는데 한참 걸렸다.. 두세시간쯤 걸린거같은데..
+// 전에는 이걸 어떻게 푼거지...
+function solution(numbers, hand) {
+  let numArr = [
+    [3, 6, 9, "#"],
+    [1, 4, 7, "*"],
+    [2, 5, 8, 0],
+  ];
+  let nowR = "#";
+  let nowL = "*";
+
+  function dis(from, to) {
+    let fromLo = [];
+    let toLo = [2, numArr[2].indexOf(to)];
+    if (from === 0) fromLo = [2, numArr[2].indexOf(from)];
+    else if (from % 3 === 0 || from === "#")
+      fromLo = [0, numArr[0].indexOf(from)];
+    else if (from % 3 === 1 || from === "*")
+      fromLo = [1, numArr[1].indexOf(from)];
+    else fromLo = [2, numArr[2].indexOf(from)];
+
+    let distance = Math.abs(fromLo[1] - toLo[1]);
+    return fromLo[0] === toLo[0] ? distance : distance + 1;
+  }
+
+  let answer = "";
+  for (let i of numbers) {
+    if (i % 3 === 0 && i !== 0) {
+      answer += "R";
+      nowR = i;
+    } else if (i % 3 === 1) {
+      answer += "L";
+      nowL = i;
+    } else {
+      if (dis(nowR, i) < dis(nowL, i)) {
+        answer += "R";
+        nowR = i;
+      } else if (dis(nowR, i) > dis(nowL, i)) {
+        answer += "L";
+        nowL = i;
+      } else {
+        if (hand === "right") {
+          answer += "R";
+          nowR = i;
+        } else {
+          answer += "L";
+          nowL = i;
+        }
+      }
+    }
+  }
+  return answer;
+}
