1bb5b4c56bd647518639c5cd4ac156288ce944f2
Subin Yook <sprout.jessie@gmail.com>
2023-08-21 18:10:45 +0900

    연속된 부분수열의 합 추가 테스트 (진행중)

--- test.js
+++ test.js
@@ -17,6 +17,7 @@ const getIndx = (n) => {
 };
 
 const testSequence = getSequence(10, -100, 100);
+console.log(testSequence);
 const testIdx = getIndx(10);
 
 // 생성한 인덱스의 합계
@@ -32,39 +33,26 @@ const testk = getK();
 function solution(sequence, k) {
   let result = [];
   let length = 1000000;
-  let start = 0;
-  let end = 0;
-  let sum = sequence[start];
+  let sum = 0;
 
-  while (end < sequence.length) {
-    if (sum === k) {
-      const nowLength = end - start;
-      if (nowLength < length) {
-        result = [start, end];
-        length = nowLength;
+  for (let start = 0; start < sequence.length; start++) {
+    let end = start;
+    sum = sequence[start];
+
+    while (sum <= k && end < sequence.length) {
+      if (sum === k) {
+        const nowLength = end - start;
+        if (nowLength < length) {
+          result = [start, end];
+          length = nowLength;
+        }
       }
-    }
-    if (sum < 0 && sequence[end] < 0) {
-      // 합계값이 음수고, 지금 더해서 비교할 수가 음수이면 음수용 처리
-      if (sum > k) {
-        end += 1;
-        sum -= Math.abs(sequence[end]);
-      } else {
-        sum += Math.abs(sequence[start]);
-        start += 1;
-      }
-    } else {
-      // 그 외 양수용 처리, sum이 양수로 넘어가는 경우 비교를 반대로 해야하기 때문에
-      if (sum < k) {
-        end += 1;
-        sum += sequence[end];
-      } else {
-        sum -= sequence[start];
-        start += 1;
-      }
+      end += 1;
+      sum += sequence[end];
     }
   }
   return result;
 }
+
 console.log(solution(testSequence, testk));
 console.log(`The answer is : [${testIdx}]`);
