8c638fd30c82d52c1d6daa3eba67f64ba177479d
Subin Yook <sprout.jessie@gmail.com>
2023-08-24 00:48:59 +0900

    연속된 부분수열의 합 문제 다시풀기

--- test.js
+++ test.js
@@ -16,7 +16,7 @@ const getIndx = (n) => {
   return [getRandomInt(1, n - 1), getRandomInt(1, n - 1)].sort((a, b) => a - b);
 };
 
-const testSequence = getSequence(6, -500, 500);
+const testSequence = getSequence(6, 1, 1000);
 const testIdx = getIndx(testSequence.length);
 
 // 생성한 인덱스의 합계
@@ -27,34 +27,36 @@ const getK = () => {
 };
 const testK = getK();
 
-// 부분 수열의 합계 구하기
+// ⭐️ 부분 수열의 합계 구하기 ⭐️
+// ⭐️ 문제를 작은단위로 분해해서 생각할 것 ⭐️
+
+// end는 0부터 마지막까지 하나씩 더하면서 확인한다
+// 더한게 k랑 같으면 길이가 더 작은 경우의 인덱스만 답에 덮어씌운다
+// 더한게 아직 k보다 작으면 start값은 바꾸지말고 다시 end를 더하도록 반복문의 처음으로 보낸다
+// sum이 k를 넘겼을때는 start값을 하나씩 빼본다
+
 function solution(sequence, k) {
   let result = [];
   let length = 1000000;
-
-  const prefix = [0];
-  sequence.map((n, i) => prefix.push(prefix[i] + n));
-  console.log("prefix :", prefix);
-
+  let sum = 0;
   let start = 0;
-  let end = prefix.length - 1;
 
-  while (start <= end) {
-    const sum = prefix[end] - prefix[start];
-    if (sum === k) {
-      const nowLength = end - 1 - start;
-      if (nowLength < length) {
-        result = [start, end - 1];
-        length = nowLength;
-      }
+  for (let end = 0; end < sequence.length; end++) {
+    sum += sequence[end];
+    for (; start <= end; start++) {
+      if (sum === k) {
+        const nowLength = end - start;
+        if (nowLength < length) {
+          result = [start, end];
+          length = nowLength;
+        }
+        break;
+      } else if (sum < k) {
+        break;
+      } else sum -= sequence[start];
     }
-    console.log("start :", start, "end :", end, "sum :", sum);
-
-    if (sum < k) {
-      if (sequence[end] < 0) end -= 1;
-      else start += 1;
-    } else end -= 1;
   }
+
   return result;
 }
 
