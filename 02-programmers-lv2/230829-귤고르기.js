d835f2402ff0c59f3972fac51202e409fb2559f8
jessie-yook <sprout.jessie@gmail.com>
2023-09-01 00:54:54 +0900

    프로그래머스 레벨2 - 귤 고르기 문제

---
+++ 02-programmers-lv2/230829-귤고르기.js
@@ -0,0 +1,18 @@
+// 연습문제 - 귤 고르기
+// https://school.programmers.co.kr/learn/courses/30/lessons/138476
+
+// 2023-08-29
+function solution(k, tangerine) {
+  const count = new Array(Math.max(...tangerine) + 1).fill(0);
+  tangerine.map((v) => (count[v] += 1));
+  count.sort((a, b) => b - a);
+
+  let sum = 0;
+  let size = 0;
+  for (const i of count) {
+    sum += i;
+    size += 1;
+    if (sum >= k) return size;
+  }
+}
+solution(6, [1, 3, 2, 5, 4, 5, 2, 3]);
