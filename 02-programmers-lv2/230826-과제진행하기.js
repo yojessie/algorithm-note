6b086bed0dcc4da02a33d9dd82d6481955c85581
jessie-yook <sprout.jessie@gmail.com>
2023-09-01 11:14:08 +0900

    과제진행하기 문제 리팩토링

--- 02-programmers-lv2/230826-과제진행하기.js
+++ 02-programmers-lv2/230826-과제진행하기.js
@@ -1,29 +1,22 @@
 // 연습문제 - 과제 진행하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/176962
 
-// 2023-08-26
+// 리팩토링 : 2023-09-01
 function solution(plans) {
   for (const i of plans) {
     i[1] = i[1].split(":").map(Number);
     i[2] = Number(i[2]);
   }
   plans.sort((a, b) => {
-    if (a[1][0] < b[1][0]) return -1;
-    if (a[1][0] > b[1][0]) return 1;
-    if (a[1][0] === b[1][0]) {
-      if (a[1][1] < b[1][1]) return -1;
-      if (a[1][1] > b[1][1]) return 1;
-      return 0;
-    }
+    if (a[1][0] === b[1][0]) return a[1][1] - b[1][1];
+    return a[1][0] - b[1][0];
   });
 
   const planMap = new Map();
   plans.map((v) => {
     const data = {
-      time: {
-        hour: v[1][0],
-        minute: v[1][1],
-      },
+      hour: v[1][0],
+      minute: v[1][1],
       play: v[2],
     };
     planMap.set(v[0], data);
@@ -32,47 +25,38 @@ function solution(plans) {
   const answer = [];
   const nameStack = [];
 
-  for (let i = 0; i < plans.length; i++) {
-    if (i === plans.length - 1) {
-      answer.push(plans[i][0]);
-      if (nameStack.length > 0) {
-        for (let j = nameStack.length - 1; j >= 0; j--)
-          answer.push(nameStack[j]);
-      }
-      break;
-    }
-
+  for (let i = 0; i < plans.length - 1; i++) {
     const now = planMap.get(plans[i][0]);
     const next = planMap.get(plans[i + 1][0]);
 
-    const hourDiff = next.time.hour - now.time.hour;
+    const hourDiff = next.hour - now.hour;
     if (hourDiff === 1) {
-      now.play -= 60 - now.time.minute + next.time.minute;
-    }
-    if (hourDiff === 0) {
-      now.play -= next.time.minute - now.time.minute;
-    }
-    if (hourDiff >= 2) {
-      now.play -= 60 * hourDiff - now.time.minute + next.time.minute;
+      now.play -= 60 - now.minute + next.minute;
+    } else if (hourDiff === 0) {
+      now.play -= next.minute - now.minute;
+    } else {
+      now.play -= 60 * hourDiff - now.minute + next.minute;
     }
 
     const callBefore = (leftMinutes) => {
       if (nameStack.length === 0) return;
 
       const before = planMap.get(nameStack.at(-1));
-      before.play -= leftMinutes;
-      if (before.play <= 0) answer.push(nameStack.pop());
-      if (before.play < 0) {
-        callBefore(Math.abs(before.play));
+      before.play += leftMinutes;
+      if (before.play <= 0) {
+        answer.push(nameStack.pop());
+        if (before.play < 0) callBefore(before.play);
       }
     };
 
-    if (now.play <= 0) answer.push(plans[i][0]);
-    if (now.play > 0) nameStack.push(plans[i][0]);
-    if (now.play < 0) {
-      callBefore(Math.abs(now.play));
-    }
+    if (now.play <= 0) {
+      answer.push(plans[i][0]);
+      if (now.play < 0) callBefore(now.play);
+    } else nameStack.push(plans[i][0]);
   }
+
+  answer.push(plans[plans.length - 1][0]);
+  while (nameStack.length > 0) answer.push(nameStack.pop());
 
   console.log(answer);
 }
@@ -83,6 +67,3 @@ solution([
   ["history", "14:00", "30"],
   ["computer", "12:30", "100"],
 ]);
-
-// 생각의 과정
-// https://buttercup-gargoyle-c70.notion.site/230826-99ee6514bba24fc99a9d304ee4b3b778?pvs=4
