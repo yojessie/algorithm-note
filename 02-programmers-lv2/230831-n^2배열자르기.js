d23d9cdb5c12eb59605559dd9d558808b01fb663
jessie-yook <sprout.jessie@gmail.com>
2023-09-06 17:30:35 +0900

    n^2 배열 자르기 문제 - 시간초과 부분 해결

--- 02-programmers-lv2/230831-n^2배열자르기.js
+++ 02-programmers-lv2/230831-n^2배열자르기.js
@@ -1,17 +1,18 @@
 // 월간 코드 챌린지 시즌3 - n^2 배열 자르기
 // https://school.programmers.co.kr/learn/courses/30/lessons/87390
 
-// 2023-08-31
-// 아래 코드는 시간초과로 실패
+// 2023-09-06
 function solution(n, left, right) {
-  const arr = new Array(n).fill().map(() => Array(n).fill(0));
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      arr[i][j] = Math.max(i, j) + 1;
+  const answer = [];
+  const startRow = Math.floor(left / n);
+  const endRow = Math.floor(right / n);
+  for (let i = startRow; i <= endRow; i++) {
+    for (let j = 0; j < n; j++) {
+      answer.push(Math.max(i, j) + 1);
     }
   }
-  const answer = [];
-  arr.map((v) => answer.push(...v));
-  return answer.slice(left, right + 1);
+  const startIdx = left % n;
+  const endIdx = answer.length - (n - (right % n));
+  return answer.slice(startIdx, endIdx + 1);
 }
 solution(3, 2, 5);
