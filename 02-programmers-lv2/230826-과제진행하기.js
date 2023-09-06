4cdfcf49ae39e0ba87b752fd3023da71d4b75106
jessie-yook <sprout.jessie@gmail.com>
2023-09-06 11:23:56 +0900

    과제진행하기 문제 리팩토링

--- 02-programmers-lv2/230826-과제진행하기.js
+++ 02-programmers-lv2/230826-과제진행하기.js
@@ -1,22 +1,21 @@
 // 연습문제 - 과제 진행하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/176962
 
-// 리팩토링 : 2023-09-01
+// 리팩토링 : 2023-09-06
 function solution(plans) {
   for (const i of plans) {
     i[1] = i[1].split(":").map(Number);
     i[2] = Number(i[2]);
   }
-  plans.sort((a, b) => {
-    if (a[1][0] === b[1][0]) return a[1][1] - b[1][1];
-    return a[1][0] - b[1][0];
+  plans.sort(([, a], [, b]) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    return a[0] - b[0];
   });
 
   const planMap = new Map();
   plans.map((v) => {
     const data = {
-      hour: v[1][0],
-      minute: v[1][1],
+      time: v[1][0] * 60 + v[1][1],
       play: v[2],
     };
     planMap.set(v[0], data);
@@ -29,29 +28,21 @@ function solution(plans) {
     const now = planMap.get(plans[i][0]);
     const next = planMap.get(plans[i + 1][0]);
 
-    const hourDiff = next.hour - now.hour;
-    if (hourDiff === 1) {
-      now.play -= 60 - now.minute + next.minute;
-    } else if (hourDiff === 0) {
-      now.play -= next.minute - now.minute;
-    } else {
-      now.play -= 60 * hourDiff - now.minute + next.minute;
-    }
-
-    const callBefore = (leftMinutes) => {
-      if (nameStack.length === 0) return;
-
-      const before = planMap.get(nameStack.at(-1));
-      before.play += leftMinutes;
-      if (before.play <= 0) {
-        answer.push(nameStack.pop());
-        if (before.play < 0) callBefore(before.play);
-      }
-    };
+    now.play -= next.time - now.time;
 
     if (now.play <= 0) {
       answer.push(plans[i][0]);
-      if (now.play < 0) callBefore(now.play);
+      if (now.play < 0) {
+        let leftTime = now.play;
+        for (let j = nameStack.length - 1; j >= 0; j--) {
+          const before = planMap.get(nameStack[j]);
+          before.play += leftTime;
+          if (before.play <= 0) {
+            answer.push(nameStack.pop());
+            if (before.play < 0) leftTime = before.play;
+          } else break;
+        }
+      }
     } else nameStack.push(plans[i][0]);
   }
 
