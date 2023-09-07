7e0178d5a662235b08fbd8c61f8ff46111ce5492
jessie-yook <sprout.jessie@gmail.com>
2023-09-07 20:18:29 +0900

    행렬의 곱셈 리팩토링 - 결국 연산한 값을 배열로 리턴하는 것이므로 map함수 활용, sum을 구할때는 reduce 활용

--- 02-programmers-lv2/230906.js
+++ 02-programmers-lv2/230906.js
@@ -3,19 +3,13 @@
 
 // 2023-09-07
 function solution(arr1, arr2) {
-  const answer = [];
-  for (const row of arr1) {
-    const result = [];
-    for (let i = 0; i < arr2[0].length; i++) {
-      let sum = 0;
-      for (let j = 0; j < arr2.length; j++) {
-        sum += row[j] * arr2[j][i];
-      }
-      result.push(sum);
-    }
-    answer.push(result);
-  }
-  return answer;
+  return arr1.map((row) => {
+    return arr2[0].map((_, i) => {
+      return row.reduce((sum, cur, j) => {
+        return sum + cur * arr2[j][i];
+      }, 0);
+    });
+  });
 }
 solution(
   [
