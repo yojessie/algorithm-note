503e0a0d5090853dce2787cc0683297a4e5cd75b
jessie-yook <sprout.jessie@gmail.com>
2023-09-08 16:46:02 +0900

    할인행사 문제 리팩토링 - 다시 생각해보니 Map 객체 없이 주어진 인자를 그대로 활용해 풀 수 있어서 수정

--- 02-programmers-lv2/230908-할인행사.js
+++ 02-programmers-lv2/230908-할인행사.js
@@ -1,22 +1,22 @@
 // 연습문제 - 할인행사
 // https://school.programmers.co.kr/learn/courses/30/lessons/131127
 
-// 2023-09-08
+// 2023-09-08 : 리팩토링
 function solution(want, number, discount) {
   let result = 0;
-  const wantMap = new Map();
-  for (let i = 0; i < want.length; i++) wantMap.set(want[i], number[i]);
 
-  for (let j = 0; j <= discount.length - 10; j++) {
-    const tenDays = discount.slice(j, j + 10);
-    let count = 0;
-    for (const item of want) {
-      const itemCount = tenDays.filter((name) => name === item).length;
-      const wantCount = wantMap.get(item);
-      if (itemCount !== wantCount) break;
-      count++;
+  for (let i = 0; i <= discount.length - 10; i++) {
+    const tenDays = discount.slice(i, i + 10);
+    let flag = true;
+    for (let idx = 0; idx < want.length; idx++) {
+      const itemCount = tenDays.filter((item) => item === want[idx]).length;
+      const wantCount = number[idx];
+      if (itemCount !== wantCount) {
+        flag = false;
+        break;
+      }
     }
-    if (count === want.length) result += 1;
+    if (flag) result += 1;
   }
   return result;
 }
