d067e8107cce8503f25b4f71fdccf98d53d7ef3d
jessie-yook <sprout.jessie@gmail.com>
2023-09-14 19:23:13 +0900

    기능개발 - 문제풀이

---
+++ 02-programmers-lv2/230913-기능개발.ts
@@ -0,0 +1,28 @@
+// 스택/큐 - 기능개발
+// https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+// 2023-09-13
+function solution(progresses: number[], speeds: number[]) {
+  const answer = [];
+  const dailyWork = (taskList: number[]) => {
+    for (let i = 0; i < taskList.length; i++) {
+      progresses[i] += speeds[i];
+    }
+  };
+
+  while (progresses.length > 0) {
+    if (progresses[0] >= 100) {
+      let count = 0;
+      for (let task of progresses) {
+        if (task < 100) break;
+        count += 1;
+      }
+      progresses.splice(0, count);
+      speeds.splice(0, count);
+      answer.push(count);
+    }
+    dailyWork(progresses);
+  }
+  return answer;
+}
+solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]);
