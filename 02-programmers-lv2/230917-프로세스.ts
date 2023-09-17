33ae04473c23f39996d26ca0763ef8838219e8ab
jessie-yook <sprout.jessie@gmail.com>
2023-09-17 23:45:25 +0900

    프로세스 - 문제풀이

---
+++ 02-programmers-lv2/230917-프로세스.ts
@@ -0,0 +1,22 @@
+// 스택/큐 - 프로세스
+// https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+// 2023-09-17
+function solution(priorities: number[], location: number) {
+  let count = 0;
+  while (priorities.length > 0) {
+    const process = priorities.shift();
+    if (process === Math.max(...priorities)) {
+      priorities.shift();
+      count += 1;
+      if (location === 0) break;
+      else location -= 1;
+    } else {
+      const process = Number(priorities.shift());
+      priorities.push(process);
+      if (location === 0) location = priorities.length;
+      location -= 1;
+    }
+  }
+  return count;
+}
